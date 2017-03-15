3945d88c240d18ad2e615f5e57ac3fe564e75479
leven.chen <leven.chen@dji.com>
2017-03-15 18:40:43 +0800

    1. 添加tab library

--- app/src/main/java/com/leven/booguubalancescale/bluetooth/fragment/BleHomeFragment.java
+++ app/src/main/java/com/leven/booguubalancescale/bluetooth/fragment/BleHomeFragment.java
@@ -61,6 +61,7 @@ public class BleHomeFragment extends SupportFragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
